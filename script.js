ee49ad501c8922b311a064f403cc2bf697e7137b
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-07 12:32:26 +0530

    Update: improve JS

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 
 let humanScore = 0, computerScore = 0;
 
+// Function to get user choice
 function getComputerChoice() {
     let computerChoice;
+    // Genrate a random number from 1 through 3
     const num = Math.floor(Math.random() * 3) + 1;
+
+    // Run switch over the number and generate the corresponding computer choice
     switch (num) {
         case 1:
             computerChoice = "Rock"
@@ -21,28 +25,62 @@ function getComputerChoice() {
     }
 }
 
+// Function to get human choice 
 function getHumanChoice() {
     const humanChoice = prompt("Enter your choice (Rock, Paper or Scissors): ").toUpperCase();
     return humanChoice;
 }
 
+// PlayRound function to check the winner
 function playRound(computerChoice, humanChoice) {
-    if (computerChoice == "Rock" && humanChoice == "SCISSORS") computerScore++;
-    else if (computerChoice == "Paper" && humanChoice == "ROCK") computerScore++;
-    else if (computerChoice == "Scissors" && humanChoice == "PAPER") computerScore++;
-    else if (computerChoice == "Scissors" && humanChoice == "ROCK") humanScore++;
-    else if (computerChoice == "Rock" && humanChoice == "PAPER") humanScore++;
-    else if (computerChoice == "Paper" && humanChoice == "SCISSORS") humanScore++;
+    if (computerChoice == "Rock" && humanChoice == "SCISSORS") {
+        computerScore++;
+    }
+    else if (computerChoice == "Paper" && humanChoice == "ROCK") {
+        computerScore++;
+        console.log(`Computer won,${computerChoice} beats ${humanChoice}`)
+    }
+    else if (computerChoice == "Scissors" && humanChoice == "PAPER") {
+        computerScore++;
+        console.log(`Computer won,${computerChoice} beats ${humanChoice}`)
+
+    }
+    else if (computerChoice == "Scissors" && humanChoice == "ROCK") {
+        humanScore++;
+        console.log(`User won, ${humanChoice} beats ${computerChoice}`);
+    }
+    else if (computerChoice == "Rock" && humanChoice == "PAPER") {
+        humanScore++;
+        console.log(`User won, ${humanChoice} beats ${computerChoice}`);
+
+    }
+    else if (computerChoice == "Paper" && humanChoice == "SCISSORS") {
+        humanScore++;
+        console.log(`User won, ${humanChoice} beats ${computerChoice}`);
+
+    }
+    // Tie case
+    else if (computerChoice == humanChoice) {
+        console.log("It's a tie");
+    }
 
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
 
 
-playRound(computerChoice, humanChoice);
+// PlayGame function that updates and runs playround function 5 times!
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(computerChoice, humanChoice);
 
-if (humanScore > computerScore) console.log("The user won!");
-else if (computerScore > humanScore) console.log("The computer won!");
-else console.log("It's a tie!");
+    }
+    if (humanScore > computerScore) console.log("The user won!");
+    else if (computerScore > humanScore) console.log("The computer won!");
+    else console.log("It's a tie!");
+}
 
+
+// Call playgame funcion!
+playGame();
